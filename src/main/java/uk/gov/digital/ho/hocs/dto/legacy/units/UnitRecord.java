2a428f69bd7325af07ae3f2782611a1bfab96d9a
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2017-11-03 00:49:56 +0000

    simplify unitRecord logic

--- src/main/java/uk/gov/digital/ho/hocs/dto/legacy/units/UnitRecord.java
+++ src/main/java/uk/gov/digital/ho/hocs/dto/legacy/units/UnitRecord.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import uk.gov.digital.ho.hocs.model.BusinessGroup;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,10 +17,7 @@ public class UnitRecord {
     private List<UnitEntityRecord> units;
 
     public static UnitRecord create(List<BusinessGroup> list) {
-        List<UnitEntityRecord> records = new ArrayList<>();
-
-        list.forEach( g -> records.add(UnitEntityRecord.create(g)));
-
+        List<UnitEntityRecord> records = list.stream().map(g -> UnitEntityRecord.create(g)).collect(Collectors.toList());
         return new UnitRecord(records);
     }
 
